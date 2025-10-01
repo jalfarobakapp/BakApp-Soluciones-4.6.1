e1a92d62466f3e40f92977ffefba41f136f772d6
jalfarobakapp <49737143+jalfarobakapp@users.noreply.github.com>
2025-10-01 12:59:07 -0300

    Actualización de consulta SQL que genera la foto stock Se crea metodo Fx_CrearFoto2

--- AntBru/My Project/AssemblyInfo.vb
+++ AntBru/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.4.6.73")>
-<Assembly: AssemblyFileVersion("3.4.6.73")>
+<Assembly: AssemblyVersion("3.4.6.74")>
+<Assembly: AssemblyFileVersion("3.4.6.74")>
